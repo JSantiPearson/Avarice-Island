40e1ef1922480f84d399466aea66d76840892ef8
Harper Shapiro <harper.shapiro@gmail.com>
2020-02-04 01:48:06 -0800

    added pause menu

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+	public bool gamePaused=false;
+	public GameObject pauseMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+    	pauseMenu.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if(Input.GetKeyDown(KeyCode.P)){
+        	if(gamePaused==false){
+        		Time.timeScale=0f;
+        		gamePaused=true;
+        		pauseMenu.SetActive(true);
+        	} else if (gamePaused==true){
+        		Time.timeScale=1f;
+        		gamePaused=false;
+        		pauseMenu.SetActive(false);
+        	}
+        }
+
+    }
+}
