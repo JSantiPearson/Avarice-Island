61f4d72d4e1a0c776957719e0e6a8935165b5931
unknown <harper.shapiro@gmail.com>
2020-02-05 17:43:07 -0800

    audio manager

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+	public bool gamePaused=false;
+	public GameObject pauseMenu;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+    	pauseMenu.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if(Input.GetKeyDown(KeyCode.P)){
+        	if(gamePaused==false){
+        		Time.timeScale=0f;
+        		gamePaused=true;
+                player.GetComponent<Hero>().enabled=false;
+        		pauseMenu.SetActive(true);
+        	} else if (gamePaused==true){
+        		Time.timeScale=1f;
+        		gamePaused=false;
+                player.GetComponent<Hero>().enabled=true;
+        		pauseMenu.SetActive(false);
+        	}
+        }
+
+    }
+}
---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+//Sound Class from Brackeys
+
+[System.Serializable]
+public class Sound {
+
+	public string name;
+
+	public AudioClip clip;
+
+	//Sound properties
+
+	[Range(0f, 1f)]
+	public float volume = .75f;
+	[Range(0f, 1f)]
+	public float volumeVariance = .1f;
+
+	[Range(.1f, 3f)]
+	public float pitch = 1f;
+	[Range(0f, 1f)]
+	public float pitchVariance = .1f;
+
+	public bool loop = false;
+
+	public AudioMixerGroup mixerGroup;
+
+	[HideInInspector]
+	public AudioSource source;
+
+}
