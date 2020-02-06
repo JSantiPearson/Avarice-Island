b68165335478c928a18c68e25e173db2d5fa6c54
Harper Shapiro <harper.shapiro@gmail.com>
2020-02-05 20:20:07 -0800

    start of branch

---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+
+  public Animator transitionAnimation;
+	public string sceneName;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKeyDown(KeyCode.Tab) && sceneName!=null){
+       	StartCoroutine(LoadScene());
+       } 
+    }
+
+    IEnumerator LoadScene(){
+      transitionAnimation.SetTrigger("end");
+      yield return new WaitForSeconds(1f);
+      SceneManager.LoadScene(sceneName);
+    }
+}
