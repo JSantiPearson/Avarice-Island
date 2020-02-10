ae024c372dc75702b5f90ba4e5ec619e54c7dee1
Harper Shapiro <harper.shapiro@gmail.com>
2020-02-09 23:04:26 -0800

    enemy counting logic

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,23 @@ public class GameManager : MonoBehaviour {
   public Hero actor;
   public bool cameraFollows = true;
   public CameraBounds cameraBounds;
+  public GameObject enemyPrefab;
 //2
   void Start() {
     cameraBounds.SetXPosition(cameraBounds.minVisibleX);
+    for(int i=0;i<3;i++){
+      Instantiate(enemyPrefab);
+    }
   }
 //3
 void Update() {
     if (cameraFollows) {
       cameraBounds.SetXPosition(actor.transform.position.x);
     }
+
+    //we can check how many enemies are left by finding all objects tagged enemy
+    //MAKE SURE ALL ENEMIES ARE INSTANTIATED WITH 'Enemy' TAGS
+    Debug.Log("Enemies: " + (GameObject.FindGameObjectsWithTag("Enemy")).Length);
   }
   
 public void LockCamera(){
