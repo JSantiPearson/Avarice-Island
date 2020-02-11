6cebb86b751162420fa97ddfb0c33c04f8b2eb75
Harper Shapiro <harper.shapiro@gmail.com>
2020-02-10 22:55:16 -0800

    enemy spawner test

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,22 @@
 using UnityEngine;
+using System.Collections;
 public class GameManager : MonoBehaviour {
   //1
   public Hero actor;
   public bool cameraFollows = true;
   public CameraBounds cameraBounds;
   public GameObject enemyPrefab;
+  //MOVE THESE ELSEWHERE WHEN SPAWNING IS REFACTORED
+  int maxEnemies=5;
+  public float spawnInterval=2;
+  public float timeElapsed=0;
+
 //2
   void Start() {
     cameraBounds.SetXPosition(cameraBounds.minVisibleX);
-    for(int i=0;i<3;i++){
-      Instantiate(enemyPrefab);
-    }
   }
 //3
-void Update() {
+  void Update() {
     if (cameraFollows) {
       cameraBounds.SetXPosition(actor.transform.position.x);
     }
@@ -21,13 +24,42 @@ void Update() {
     //we can check how many enemies are left by finding all objects tagged enemy
     //MAKE SURE ALL ENEMIES ARE INSTANTIATED WITH 'Enemy' TAGS
     Debug.Log("Enemies: " + (GameObject.FindGameObjectsWithTag("Enemy")).Length);
+
+    //test for interval spawns
+    timeElapsed+=Time.deltaTime;
+    if(okToSpawn()){
+      Instantiate(enemyPrefab);
+    }
+
   }
   
-public void LockCamera(){
-  cameraFollows=false;
-}
+  public void LockCamera(){
+   cameraFollows=false;
+  }
 
-public void unlockCamera(){
-  cameraFollows=true;
-}
+  public void unlockCamera(){
+    cameraFollows=true;
+  }
+
+  //NEED TO MOVE FOLLOWING METHODS EVENTUALLY. NEED A SMART SPAWNING SCHEME
+
+  public bool okToSpawn(){
+    if(timeElapsed>spawnInterval && GameObject.FindGameObjectsWithTag("Enemy").Length<maxEnemies){
+      timeElapsed=0;
+      return true;
+    } else {
+      return false;
+    }
+  } 
+  /**
+  IEnumerator SpawnAndWait(){
+    Instantiate(enemyPrefab);
+    yield return new WaitForSeconds(2);
+  }
+
+  IEnumerator SpawnEnemies(int numEnemies){
+    for(int i=0;i<numEnemies;i++){
+      StartCoroutine("SpawnAndWait");
+    }
+  }**/
 }
