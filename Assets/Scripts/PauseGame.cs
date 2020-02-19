28e0b31622aab94de346ad2be4a21c777afe64bc
Harper Shapiro <harper.shapiro@gmail.com>
2020-02-18 22:08:29 -0800

    options menu within pause menu

--- Assets/Scripts/PauseGame.cs
+++ Assets/Scripts/PauseGame.cs
@@ -6,12 +6,13 @@ public class PauseGame : MonoBehaviour
 {
 	public bool gamePaused=false;
 	public GameObject pauseMenu;
+	public GameObject optionsMenu;
     public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
     	pauseMenu.SetActive(false);
-
+    	optionsMenu.SetActive(false);
     }
 
     // Update is called once per frame
@@ -19,17 +20,35 @@ public class PauseGame : MonoBehaviour
     {
          if(Input.GetKeyDown(KeyCode.P)){
         	if(gamePaused==false){
-        		Time.timeScale=0f;
-        		gamePaused=true;
-                player.GetComponent<Hero>().enabled=false;
-        		pauseMenu.SetActive(true);
+        		Pause();
         	} else if (gamePaused==true){
-        		Time.timeScale=1f;
-        		gamePaused=false;
-                player.GetComponent<Hero>().enabled=true;
-        		pauseMenu.SetActive(false);
+        		Unpause();
         	}
         }
 
+    }
+
+    public void Pause(){
+    	Time.timeScale=0f;
+        gamePaused=true;
+        player.GetComponent<Hero>().enabled=false;
+        pauseMenu.SetActive(true);
+    }
+
+    public void Unpause(){
+    	Time.timeScale=1f;
+        gamePaused=false;
+        player.GetComponent<Hero>().enabled=true;
+        pauseMenu.SetActive(false);
+    }
+
+    public void RevealOptions(){
+    	pauseMenu.SetActive(false);
+    	optionsMenu.SetActive(true);
+    }
+
+    public void UnRevealOptions(){
+    	pauseMenu.SetActive(true);
+    	optionsMenu.SetActive(false);
     }
 }
