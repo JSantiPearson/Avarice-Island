348af75c7f1c795ae10b834a4af473fe29344a03
Harper Shapiro <harper.shapiro@gmail.com>
2020-03-10 23:55:38 -0700

    dialogue for han lao

---
+++ Assets/Scripts/DefeatedBehavior.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefeatedBehavior : StateMachineBehaviour
+{
+    public Dialogue dialogue;
+    public GameObject hanLaoObject; //might need to clean this up
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        hanLaoObject = GameObject.Find("HanLao");
+        dialogue = hanLaoObject.GetComponent(typeof(Dialogue)) as Dialogue;
+        dialogue.PlayDialogue();
+
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       Destroy(hanLaoObject);
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
