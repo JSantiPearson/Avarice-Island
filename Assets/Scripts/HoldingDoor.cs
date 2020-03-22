fad835b63784be0db665d4087d747e9ddf138553
unknown <harper.shapiro@gmail.com>
2020-03-22 16:33:35 -0700

    work on bar door spawn scheme

---
+++ Assets/Scripts/HoldingDoor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldingDoor : MonoBehaviour
+{
+
+	public Animator doorAnim;
+	public SpawnPoint spawnPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(spawnPoint.spawnTriggered){
+        	doorAnim.SetTrigger("Spawn");
+        	Destroy(gameObject);
+        }
+    }
+}
