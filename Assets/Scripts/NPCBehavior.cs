29b646f8f33b6b7fef6a009334ace215947c4853
Jordan Pearson <JSantiPearson@gmail.com>
2020-03-30 13:53:50 -0700

    Add NPC running logic

---
+++ Assets/Scripts/NPCBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCBehavior : MonoBehaviour
+{
+    Hero script;
+    Vector3 transform;
+    GameObject player;
+    Animator animator;
+    void Start(){
+      player = GameObject.Find("Player");
+      script = player.GetComponent<Hero>();
+      animator = gameObject.GetComponent<Animator>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+      transform = gameObject.transform.position;
+      float playerX = player.transform.position.x;
+      if (script.engaged.Count > 0){
+        if (transform.x < playerX && transform.x > playerX-10){
+          animator.SetBool("Running", true);
+          transform.x -= 0.04f;
+          if (transform.x < playerX-10){
+            Destroy(gameObject);
+          }
+        }
+        else if(transform.x > playerX && transform.x < playerX+10){
+          animator.SetBool("Running", true);
+          transform.x += 0.04f;
+          if (transform.x > playerX+10){
+            Destroy(gameObject);
+          }
+        }
+        gameObject.transform.position = transform;
+      }
+    }
+}
