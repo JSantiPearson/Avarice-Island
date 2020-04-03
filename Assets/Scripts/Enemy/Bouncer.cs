b91ae4bd67facb91b8a9c58ec31f0ce474a0cae3
unknown <harper.shapiro@gmail.com>
2020-04-03 15:28:44 -0700

    more bouncer setup

--- Assets/Scripts/Enemy/Bouncer.cs
+++ Assets/Scripts/Enemy/Bouncer.cs
@@ -5,14 +5,33 @@ using UnityEngine;
 public class Bouncer : Enemy
 {
     // Start is called before the first frame update
+
+	public string bouncerName;
+
     void Start()
-    {
-        PUNCH_ANIM = "enemy_grunt_punch";
-        LAUNCH_ANIM = "enemy_grunt_launch";
-        GROUNDED_ANIM = "enemy_grunt_grounded";
-        STAND_ANIM = "enemy_grunt_stand";
-        HURT_GROUNDED_ANIM = "enemy_grunt_hurt_grounded";
-        HURT_STANDING_ANIM = "enemy_grunt_hurt_standing";
+    {	
+    	//the 2 distinct bouncers have distinct anims. this would be cleaner
+    	//if we made them clones or if we somehow changed the CheckAnims() scheme in enemy superclass
+    	//these animators also have multiple animations that correspond to a single state here. i just used one for each.
+    	switch(bouncerName){
+    		case "BouncerRex":
+        		PUNCH_ANIM = "BradPunchAnim";
+        		LAUNCH_ANIM = "BradLaunchedAnim";
+        		GROUNDED_ANIM = "BradHurtGroundedAnim"; //no regular grounded anim?
+        		STAND_ANIM = "BradGetUpAnim";
+        		HURT_GROUNDED_ANIM = "BradHurtGroundedAnim";
+        		HURT_STANDING_ANIM = "BradHurtFrontAnim";
+        		break;
+
+        	case "BouncerBrad":
+        		PUNCH_ANIM = "RexPunchAnim";
+        		LAUNCH_ANIM = "RexLaunchedAnim";
+        		GROUNDED_ANIM = "RexHurtGroundedAnim"; //no regular grounded anim?
+        		STAND_ANIM = "RexGetUpAnim";
+        		HURT_GROUNDED_ANIM = "RexHurtGroundedAnim";
+        		HURT_STANDING_ANIM = "RexHurtFrontAnim";
+        		break;
+    	}
 
 
         targetPosition = new Vector3(body.position.x, startingPosition.y, startingPosition.z);
@@ -22,6 +41,10 @@ public class Bouncer : Enemy
         currentHealth = maxHealth;
         isWaiting = true;
         fleeHealth = 30;
+
+
+
+
     }
 
     // Update is called once per frame
