1f135b239340260dd2daf071dfabb2a598c563ff
unknown <harper.shapiro@gmail.com>
2020-04-16 18:05:12 -0700

    split health bar components

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -7,25 +7,41 @@ public class Health : MonoBehaviour
 {
   public int health, numOfPips;
 
-  public Image[] pips;
+  public GameObject[] fullPips;
 
   public Sprite fullPip, emptyPip;
 
   public GameObject fullPipPrefab, emptyPipPrefab;
+  public GameObject healthBar, emptyHealthSet, fullHealthSet;
 
   void Start(){
+
+      //find health bar objects
+      healthBar = GameObject.Find("HealthBar2"); //RENAME WHEN HEALTH BAR NAME CHANGES
+      fullHealthSet = healthBar.transform.GetChild(1).gameObject; //first child
+      emptyHealthSet = healthBar.transform.GetChild(0).gameObject; //second child
+      fullPips = new GameObject[numOfPips];
+
     for(int i=0;i<numOfPips;i++){
-      Instantiate(emptyPipPrefab,transform);
+      //fill empty slot
+      Instantiate(emptyPipPrefab,emptyHealthSet.transform);
+      //fill full slot
+      GameObject currPip = Instantiate(fullPipPrefab,fullHealthSet.transform);
+      fullPips[i] = currPip;
     }
   }
 
   void Update(){
      //OLD VERSION, INTERACTS WITH ORIGINAL HEALTH BAR
-    /*
-    if (health > numOfPips)
+    
+    if (health > numOfPips){
       health = numOfPips;
+    }
 
-    for (int i = 0; i < pips.Length; i++) {
+    
+
+
+    /*for (int i = 0; i < pips.Length; i++) {
 
       if (i < health){
         pips[i].sprite = fullPip;
@@ -40,8 +56,8 @@ public class Health : MonoBehaviour
       else {
         pips[i].enabled = false;
       }
-    }
-    */
+    }*/
+    
 
 
   }
