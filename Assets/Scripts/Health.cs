1ba5ea7955470cb2784d10dea104f6eb4d75e61a
unknown <harper.shapiro@gmail.com>
2020-04-19 15:43:51 -0700

    new health bar functionality

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
-  public int health, numOfPips;
+  public int health, numOfPips; //pip behavior will be automatically scaled
 
   public GameObject[] fullPips;
 
@@ -14,15 +14,31 @@ public class Health : MonoBehaviour
   public GameObject fullPipPrefab, emptyPipPrefab;
   public GameObject healthBar, emptyHealthSet, fullHealthSet;
 
+  public GameObject playerObject;
+  public Hero player;
+
+  //reusable anim variable used in update 
+  private Animator currPipAnimator;
+
+
   void Start(){
 
+      //player references
+      playerObject = GameObject.Find("Player");
+      player = playerObject.GetComponent<Hero>();
+
+
       //find health bar objects
-      healthBar = GameObject.Find("HealthBar2"); //RENAME WHEN HEALTH BAR NAME CHANGES
+      healthBar = GameObject.Find("HealthBar"); //RENAME WHEN HEALTH BAR NAME CHANGES
       fullHealthSet = healthBar.transform.GetChild(1).gameObject; //first child
       emptyHealthSet = healthBar.transform.GetChild(0).gameObject; //second child
       fullPips = new GameObject[numOfPips];
 
-    for(int i=0;i<numOfPips;i++){
+      //health based on hero script health
+      health = (int)Hero.maxHealth; //cast shouldn't matter, should probably be const int in Hero's definition anyway
+
+    //FILL THE BAR 
+    for(int i=numOfPips-1;i>=0;i--){
       //fill empty slot
       Instantiate(emptyPipPrefab,emptyHealthSet.transform);
       //fill full slot
@@ -32,32 +48,21 @@ public class Health : MonoBehaviour
   }
 
   void Update(){
-     //OLD VERSION, INTERACTS WITH ORIGINAL HEALTH BAR
-    
-    if (health > numOfPips){
-      health = numOfPips;
-    }
-
     
 
+    int numPipsActive = (int) Mathf.Ceil(player.currentHealth / (Hero.maxHealth / numOfPips)); //(player.currentHealth/Hero.maxHealth)*numOfPips;
 
-    /*for (int i = 0; i < pips.Length; i++) {
 
-      if (i < health){
-        pips[i].sprite = fullPip;
-      }
-      else {
-        pips[i].sprite = emptyPip;
-      }
+    for(int i=0; i<numOfPips;i++){
 
-      if (i < numOfPips) {
-        pips[i].enabled = true;
+      currPipAnimator = fullPips[i].GetComponent<Animator>(); 
+
+      if(i<numPipsActive){
+        //currPipAnimator.SetTrigger("Regain"); //in case we implement regaining life points
+      } else {
+        currPipAnimator.SetTrigger("Flash"); //disables pip after flash
       }
-      else {
-        pips[i].enabled = false;
-      }
-    }*/
-    
+    }    
 
 
   }
