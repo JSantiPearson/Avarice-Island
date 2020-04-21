dac93e0fa210dbefc79694757ec399a6ff081bc3
Ethan Holle <ethanh20@hotmail.com>
2020-04-21 05:56:15 -0700

    blaster animation events

--- Assets/Scripts/Enemy/Blaster/BlasterAnimatorEventHandler.cs
+++ Assets/Scripts/Enemy/Blaster/BlasterAnimatorEventHandler.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class BlasterAnimatorEventHandler : MonoBehaviour
 {
+    public GameObject fireballProjectile;
+    public float fireballForce;
+    public GameObject fireballSpawnPoint;
+
     public void activateHitBox1(int activate)
     {
         transform.parent.GetChild(2).GetComponent<Hitbox>().SetActive(activate != 0);
+    }
+
+    public void shoot()
+    {
+        var fireball = Instantiate(fireballProjectile, fireballSpawnPoint.transform.position, transform.rotation);
+        fireball.GetComponent<Rigidbody>().AddForce(fireballSpawnPoint.transform.up * fireballForce);
     }
 }
