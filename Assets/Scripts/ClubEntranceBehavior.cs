c70740dc3d0b0403a57ef69f319745c73668fd9f
Jordan Pearson <JSantiPearson@gmail.com>
2020-05-08 21:53:53 -0700

    Fix visual bugs regarding doors

---
+++ Assets/Scripts/ClubEntranceBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClubEntranceBehavior : MonoBehaviour
+{
+  public GameObject player;
+  Vector3 playerPosition;
+  SpriteRenderer sprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+      player = GameObject.Find("Player");
+      sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      playerPosition = player.transform.position;
+      if (playerPosition.z > -1.05){
+        sprite.sortingOrder = 6;
+      }
+      else {
+        sprite.sortingOrder = 5;
+      }
+    }
+}
